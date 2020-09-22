5ceda373832bbf99aa037c8196238b31a86c14d1
Cody Cooper <codycooper9@gmail.com>
2020-09-21 22:34:30 -0700

    initial

---
+++ game.js
@@ -0,0 +1,11 @@
+var gamePattern = [];
+gamePattern.push(randomChosenColor);
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+};
+
+var randomChosenColor = buttonColors[randomNumber];
+
